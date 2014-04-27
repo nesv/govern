014205f4ecfce4667fad6efc5501e267c44831f4
Nick Saika <nicksaika@gmail.com>
2014-04-27 01:03:15 -0400

    Fiddled with the YAML parser a bit more

--- yaml.go
+++ yaml.go
@@ -6,12 +6,41 @@ import (
 	yaml "gopkg.in/yaml.v1"
 )
 
-func LoadYAML(path string, v interface{}) error {
+type (
+	RawYAML map[string]interface{}
+)
+
+func LoadYAML(path string) (RawYAML, error) {
+	var y RawYAML
 	b, err := ioutil.ReadFile(path)
 	if err != nil {
-		return nil
+		return y, err
 	}
 
-	err = yaml.Unmarshal(b, v)
-	return err
+	err = yaml.Unmarshal(b, &y)
+	return y, err
+}
+
+func (y RawYAML) ToString(key string) string {
+	if v, found := y[key]; found {
+		if s, ok := v.(string); ok {
+			delete(y, key)
+			return s
+		}
+	}
+	return ""
+}
+
+func (y RawYAML) ToStringSlice(key string) []string {
+	if v, found := y[key]; found {
+		if s, ok := v.([]string); ok {
+			delete(y, key)
+			return s
+		}
+	}
+	return nil
+}
+
+func (y RawYAML) Length() int {
+	return len(y)
 }
