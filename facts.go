6042467751d69dead215943f3b38e08303ad6fb2
Nick Saika <nicksaika@gmail.com>
2014-05-22 17:32:45 -0400

    Started on network interface facts.

--- facts.go
+++ facts.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"errors"
+	"net"
 	"os/exec"
 	"strings"
 )
@@ -22,10 +23,10 @@ type (
 	}
 
 	NetworkFacts struct {
-		Interfaces []NetworkInterfaceFacts
+		Interfaces []NetworkInterfaceFact
 	}
 
-	NetworkInterfaceFacts struct {
+	NetworkInterfaceFact struct {
 		Name string
 		Addr []string
 		MAC  string
@@ -39,16 +40,45 @@ func GatherFacts() (facts *Facts, err error) {
 		}
 	}()
 
+	// Gather operating system facts.
 	osFacts := OperatingSystemFacts{
 		Version: GetFact(CmdOSVersion),
 		Arch:    GetFact(CmdOSArch),
 		Name:    GetFact(CmdOSName)}
 
+	// Gather networking facts.
+	var interfaces []net.Interface
+	interfaces, err = net.Interfaces()
+	if err != nil {
+		return
+	}
+	ifaceFacts := make([]NetworkInterfaceFact, len(interfaces))
+	for i, iface := range interfaces {
+		fact := NetworkInterfaceFact{
+			Name: iface.Name,
+			MAC:  iface.HardwareAddr.String()}
+
+		// Get all of the addresses for the network interface.
+		var addrs []net.Addr
+		if addrs, err = iface.Addrs(); err != nil {
+			return
+		}
+		ifaddrs := make([]string, len(addrs))
+		for _, addr := range addrs {
+			ifaddrs = append(ifaddrs, addr.String())
+		}
+		fact.Addr = ifaddrs
+
+		ifaceFacts[i] = fact
+	}
+
+	// Put it all together, and whaddaya get?
 	facts = &Facts{
 		Hostname:   GetFact(CmdHostname),
 		FQDN:       GetFact(CmdFQDN),
 		DomainName: "",
-		OS:         osFacts}
+		OS:         osFacts,
+		Net:        NetworkFacts{Interfaces: ifaceFacts}}
 	return
 }
 
